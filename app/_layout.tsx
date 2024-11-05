a5ee995e8459032b66ec5234faae418d4d102622
dahalSabina <dahasa01@luther.edu>
2024-11-05 09:58:44 -0600

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,6 @@
+
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
---
+++ constants/project_images.ts
@@ -0,0 +1,10 @@
+import parachuteIcon from "@/assets/project_images/image1.png";
+import featherImage from "@/assets/project_images/feather.png";
+import worldMapIcon from "@/assets/project_images/image2.png";
+
+export default [
+    parachuteIcon,
+    featherImage,
+    worldMapIcon,
+    
+];
