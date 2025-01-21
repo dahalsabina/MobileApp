1f42f7df296a1ef76f95d247bb9aefccc2b09e9a
milliewang <slwangpwa@gmail.com>
2025-01-21 11:09:28 -0600

    progressing

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,15 +1,81 @@
 import{ ButtonCompo }from '@/components/ButtonCompo';
 import{ InputCompo }from '@/components/InputCompo';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
 import { useFonts } from 'expo-font';
-import * as Google from 'expo-auth-session/providers/google';
 import Constants from 'expo-constants';
 import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google"
+import { panHandlerName } from 'react-native-gesture-handler/lib/typescript/handlers/PanGestureHandler';
+
+// process.env.GOOGLE_CLIENT_ID;
+const iosClientId = "359637781473-fgkpsomsiavl1mon0q2lkt3orf9ijstg.apps.googleusercontent.com"
+const webClientId = "359637781473-6081rnkqg86p0mbmkvovlqpcjouc9jt6.apps.googleusercontent.com"
+const androidClientId = ""
+
+WebBrowser.maybeCompleteAuthSession()
+
 
 const SigninPage = () => {
+  const config = {
+    webClientId,
+    iosClientId,
+    androidClientId,
+  }
+
+  // Google Authentication Hook
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: webClientId,
+    iosClientId: iosClientId,
+    androidClientId: androidClientId,
+    redirectUri: AuthSession.makeRedirectUri({
+      native: "com.boidtravel:/oauthredirect"
+    })
+  });
+
+  // const redirectUri = AuthSession.makeRedirectUri({
+  //   // Specify the useProxy option directly in the context of the method
+  //   path: '', // Optional: Use if you need a specific path
+  //   preferLocalhost: false, // Ensures the Expo proxy is used when available
+  // });
+  // console.log('Redirect URI:', redirectUri);
+
+  const handleToken = () => {
+    if(response?.type === "success") {
+      const {authentication} = response;
+      const token = authentication?.accessToken;
+      console.log("access token", token)
+    }
+  }
+
+  useEffect(() => {
+    handleToken();
+  }, [response])
+  
+  // const handleGoogleSignIn = async () => {
+  //   try {
+  //     const result = await promptAsync();
+  //     if (result.type === 'success') {
+  //       const { authentication } = result;
+  
+  //       // Mocking backend call
+  //       Alert.alert('Signed in successfully! Token:', authentication?.accessToken);
+  //       router.push('/homePage');
+  //     } else if (result.type === 'dismiss') {
+  //       Alert.alert('Sign-in dismissed');
+  //     }
+  //   } catch (error) {
+  //     console.error('Google Sign-In Error:', error);
+  //     Alert.alert('Google Sign-In Error', error.message || 'An unexpected error occurred.');
+  //   }
+  // };
+
+// /////////////////////////////////////////////////////////////////////////////
+
+
   const handlePress = () => {
     // google authentication
     Alert.alert('google authentication');
@@ -21,42 +87,6 @@ const SigninPage = () => {
     Poppins: require('../assets/fonts/Poppins-Regular.ttf'),
     SemiBoldPoppins: require('../assets/fonts/Poppins-SemiBold.ttf')
   });
-
-  const iosGoogleClientId = 
-  // process.env.GOOGLE_CLIENT_ID;
-  "359637781473-fgkpsomsiavl1mon0q2lkt3orf9ijstg.apps.googleusercontent.com"
-
-  // Google Authentication Hook
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: '',
-    iosClientId: iosGoogleClientId,
-    webClientId: '',
-  });
-
-  const redirectUri = AuthSession.makeRedirectUri({
-    // Specify the useProxy option directly in the context of the method
-    path: '', // Optional: Use if you need a specific path
-    preferLocalhost: false, // Ensures the Expo proxy is used when available
-  });
-  console.log('Redirect URI:', redirectUri);
-  
-  const handleGoogleSignIn = async () => {
-    try {
-      const result = await promptAsync();
-      if (result.type === 'success') {
-        const { authentication } = result;
-  
-        // Mocking backend call
-        Alert.alert('Signed in successfully! Token:', authentication?.accessToken);
-        router.push('/homePage');
-      } else if (result.type === 'dismiss') {
-        Alert.alert('Sign-in dismissed');
-      }
-    } catch (error) {
-      console.error('Google Sign-In Error:', error);
-      Alert.alert('Google Sign-In Error', error.message || 'An unexpected error occurred.');
-    }
-  };
   
 
   return (
@@ -88,7 +118,7 @@ const SigninPage = () => {
           <View style={styles.signinContainer}>
             <Text style={styles.haveAccount}>Don't have an account yet?</Text>
             <TouchableOpacity
-              onPress={handleGoogleSignIn} 
+              onPress={() => promptAsync()}
               activeOpacity={0.7}>
             <Text style={styles.signup}>Sign up with Google</Text>
           </TouchableOpacity>
