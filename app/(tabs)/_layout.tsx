c06b36adae2b1f5e4bdd0328e342dcf9455b5ed1
milliewang <slwangpwa@gmail.com>
2025-04-12 21:16:30 -0500

    change profilePage -> profile to match other namings

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -94,7 +94,7 @@ export default function Page() {
 
             {/* Profile Tab */}
             <Tabs.Screen
-                name="profilePage"
+                name="profile"
                 options={{
                     tabBarLabel: "",
                     /**
